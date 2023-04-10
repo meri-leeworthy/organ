e19cbeb0f7ef496c8659b28ecf74f3d248506b59
meri-leeworthy <meri.leeworthy@gmail.com>
2023-04-10 14:26:57 +1000

    expo-secure-store

--- lib/useMatrixClient.ts
+++ lib/useMatrixClient.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
 import * as sdk from "matrix-js-sdk";
+import * as SecureStore from "expo-secure-store";
 
 export default function useMatrixClient() {}
