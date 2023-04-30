310ffad909dfdb7dec611cf3edefc474d1b4df45
meri-leeworthy <meri.leeworthy@gmail.com>
2023-04-30 15:43:27 +1000

    work sans config

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -20,6 +20,13 @@ export default function useCachedResources() {
         await Font.loadAsync({
           ...FontAwesome.font,
           "space-mono": require("../assets/fonts/SpaceMono-Regular.ttf"),
+          "work-sans": require("../assets/fonts/worksans/WorkSans-Regular.ttf"),
+          "work-sans-bold": require("../assets/fonts/worksans/WorkSans-Bold.ttf"),
+          "work-sans-semibold": require("../assets/fonts/worksans/WorkSans-SemiBold.ttf"),
+          "work-sans-medium": require("../assets/fonts/worksans/WorkSans-Medium.ttf"),
+          "work-sans-light": require("../assets/fonts/worksans/WorkSans-Light.ttf"),
+          "work-sans-thin": require("../assets/fonts/worksans/WorkSans-Thin.ttf"),
+          "work-sans-italic": require("../assets/fonts/worksans/WorkSans-Italic.ttf"),
         });
 
         const parsedMatrixRooms = await getAsyncStorage("matrixRooms");
