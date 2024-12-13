c4461351eb44f4706f4f2f8030cfad7fcf3331fd
Meri Leeworthy <mlee0167@student.monash.edu>
2024-12-13 16:42:42 +1100

    hbsp (partials)

--- astro/src/lib/types.tsx
+++ astro/src/lib/types.tsx
@@ -1,11 +1,11 @@
 export interface FileData<T = string> {
   name: string
   content: T
-  type: "css" | "hbs" | "md" | "asset"
+  type: "css" | "hbs" | "hbsp" | "md" | "asset"
 }
 
 export interface SelectedFile {
   activeFile: string
-  type: "css" | "hbs" | "md" | "asset"
+  type: "css" | "hbs" | "hbsp" | "md" | "asset"
   contentFile: string
 }
